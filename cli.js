f4b087a78b80b91d609c53cb71e302a744e12f5c
Muhammad Aaqil <aaqilcs102@gmail.com>
2024-04-21 09:16:47 +0500

    initial
    Signed-off-by: Muhammad Aaqil <aaqilcs102@gmail.com>

---
+++ cli.js
@@ -0,0 +1,43 @@
+const yargs = require('yargs/yargs');
+const { execute, parseGlobalPackages } = require('./utils');
+
+module.exports = async () => {
+    const extensions = ['cache', 'fuzzy', 'sql-controller'];
+
+    const { argv } = yargs(process.argv.slice(2));
+    const command = argv['_'][0];
+
+    let arguments = '';
+
+    delete argv['_'];
+    delete argv['$0'];
+
+
+    Object.keys(argv).forEach(arg => {
+        arguments += `--${arg}="${argv[arg]}" `
+    });
+    try {
+        const resposne = await execute('npm list -g --depth 0');
+        const packages = await parseGlobalPackages(resposne.stdout);
+
+        let exists = false;
+
+        if (extensions.includes(command)) {
+            packages.forEach(package => {
+                if (package.includes(`lb4-${command}`)) { exists = true; }
+            });
+            if (!exists) await execute(`npm install -g lb4-${command}`);
+            console.log(`runnning lb4-${command} ${arguments}`);
+            await execute(`lb4-${command} ${arguments}`);
+        } else {
+            packages.forEach(package => {
+                if (package.includes('@loopback/cli')) { exists = true; }
+            });
+            if (!exists) await execute('npm install -g @loopback/cli');
+            console.log(`runnning lb4 ${command} ${arguments}`);
+            await execute(`lb4 ${command} ${arguments}`);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
---
+++ utils.js
@@ -0,0 +1,32 @@
+const { exec } = require('child_process');
+
+const execPromise = (command) => {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            resolve({ error, stdout, stderr });
+        });
+    });
+}
+
+module.exports.execute = async (command, message) => {
+    const executed = await execPromise(command);
+    if (executed.error) {
+        console.log(executed.error);
+        throw Error(`failed to execute ${command}`);
+    }
+    console.log('OK');
+    return executed;
+}
+
+module.exports.parseGlobalPackages = async (stdout) => {
+    const packages = [];
+    const lines = stdout.split('\n');
+
+    lines.forEach(line => {
+        if (line.trim() === '') return;
+        const [packageName] = line.split(' -> ');
+        if (packageName) packages.push(packageName)
+    });
+
+    return packages;
+}
